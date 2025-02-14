718f4c1956538bebdb86d5a719d2f087cf19bc77
Bharath D <bharath622005@gmail.com>
2025-02-14 13:27:33 +0530

    Script Tags

---
+++ Script/script.js
@@ -0,0 +1,28 @@
+// console.log("Hello World")
+object2={}
+object2["training"]=["Git","Github","HTML","CSS","JS","MERN","React","Express","MangoDB","Hosting"]
+// console.log(object2);
+object3=new Object()
+// console.log(object3);
+object3.training="MERN"
+object3.DEPT=["CSE","IT","ADS","ECE"]
+// console.log(object3.DEPT)
+
+set =new Set(["Hello"])
+// console.log(set);
+set.add("Hi")
+// console.log(set);
+
+// Logical AND Operator
+console.log(5 && 10)
+console.log(true && 10)
+console.log(false && 10)
+console.log(false && true)
+console.log(true&&false)
+
+// Logical OR Operator
+console.log(5 || 10)
+console.log(true || 10)
+console.log(false || 10)
+console.log(false || true)
+console.log(true || false)
