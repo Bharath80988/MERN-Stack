6420e7ddd8b53a891d72ffdd638fa4c1cd76a71f
Bharath D <bharath622005@gmail.com>
2025-02-17 12:04:10 +0530

    Intermediate JS

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,4 @@
+/*
 // console.log("Hello World")
 object2={}
 object2["training"]=["Git","Github","HTML","CSS","JS","MERN","React","Express","MangoDB","Hosting"]
@@ -35,3 +36,46 @@ for(i in Array){
 var testArrowFunction = () => {
     console.log("This is the test arrow Function")
 }
+
+testArrowFunction();
+
+var a=10;
+console.log(a);
+{
+    let a=20;
+    console.log(a)
+    let b=30
+    console.log(b)
+    let c=40;
+    console.log(c)
+    var e=50;
+    console.log(a)
+    f=10;
+}
+
+console.log(a)
+console.log(f)
+
+a=11
+console.log((a%2) ? "Odd":"Even")
+*/
+
+// var arr=[10,20,30,40,50]
+// var [a,b,c,d,e,f]=arr
+// console.log(a,b,c,d,e,f)
+
+// Hoisting (Variable and Functional)
+// console.log(a);
+// let a=10;
+
+//
+
+class ClassEG{
+    classfun=()=> {
+        console.log("Hello")
+        return 1.56
+    }
+}
+
+obj = new ClassEG()
+console.log(obj.classfun())
