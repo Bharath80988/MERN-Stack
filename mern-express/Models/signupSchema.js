fc2bb076a7df19be0da685b3c5b0466d578ea279
Bharath D <bharath622005@gmail.com>
2025-02-26 08:27:07 +0530

    Express JS

---
+++ mern-express/Models/signupSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const signupSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: Number, required: true, unique: true }
+});
+
+const Signup = mongoose.model("Signup", signupSchema);
+
+module.exports = Signup; 
--- mern-express/index.js
+++ mern-express/index.js
@@ -1,21 +1,46 @@
 const express = require("express");
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
+const Signup = require("./Models/signupSchema");
+
 
 const app = express();
-
-app.get("/",(req,res)=>{
-    res.send("<h1> Welcome to Backend Server </h1>")
-})
-
-app.get("/Home",(req,res)=>{
-    res.send("<h1> Welcome to Home </h1>")
-})
-
-app.get("/static",(req,res)=>{
-    res.sendFile("E:\\MERN\\index.html")
-})
-
-
-
+app.use(express.json());
 const PORT = 3001;
+dotenv.config();
 
-app.listen(PORT, () => console.log("Server started Successfully"));
+// console.log("mongodb+srv://admin:Bharath@2005@mernbharath.mhkgb.mongodb.net/")
+
+mdb
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongo DB connection successfull");
+  })
+  .catch((err) => {
+    console.log("Check your connection string", err);
+  });
+
+app.get("/static", (req, res) => {
+  res.sendFile("D:\\MERN_Stack\\HTML_CSS\\index.html");
+});
+
+app.post("/signup", (req, res) => {
+  try {
+    const { firstName, lastName, email, password, phoneNumber } = req.body;
+    const newSignup = new Signup({
+      firstName: "Bharath",
+      lastName: "D",
+      email: "bharath.com",
+      password: "sjit",
+      phoneNumber: 9344816453,
+    });
+    newSignup.save();
+    console.log("Signup successful");
+    res.status(201).json({ message: "signup successfull", isSignup: true });
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({ message: "signup unsuccessfull", isSignup: false });
+  }
+});
+
+app.listen(PORT, () => console.log("Server Started successfully."));
